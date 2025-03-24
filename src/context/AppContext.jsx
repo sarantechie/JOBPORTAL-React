1297f23b659b5833e0c2fcf465d11b2d763ce0f1
sarantechie <saran.tech25@gmail.com>
2025-03-24 15:26:51 +0530

    vercel deployment

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -13,7 +13,7 @@ export const AppProvider = ({ children }) => {
     const fetch = async () => {
       if (token) {
         await axios
-          .get("http://localhost:5000/api/auth/me", {
+          .get("https://jobportalapi.vercel.app/api/auth/me", {
             headers: { Authorization: `Bearer ${token}` },
           })
           .then((res) => {
@@ -34,7 +34,7 @@ export const AppProvider = ({ children }) => {
 
   const googleLogin = async (googleToken) => {
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/google-login", { token: googleToken });
+      const res = await axios.post("https://jobportalapi.vercel.app/api/auth/google-login", { token: googleToken });
 
       setToken(res.data.token);
       localStorage.setItem("token", res.data.token);
